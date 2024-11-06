230beeb36db73ca458eb8ef991fbab29a8a7eab3
Thiago Elias <thiagoelias99@gmail.com>
2024-11-06 14:23:59 -0300

    feat: fim da aula 2

---
+++ finance-ai-next/app/transactions/page.tsx
@@ -0,0 +1,19 @@
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
+import AddTransactionButton from "../_components/add-transaction-button";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <AddTransactionButton />
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
+};
+
+export default TransactionsPage;
